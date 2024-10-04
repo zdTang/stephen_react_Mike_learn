fd50eb2b6616c982b25a4fab930e53617b60d74c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-04 15:33:49 -0400

    c5-init react project

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,28 +1,12 @@
-import './App.css';
-import { useState } from 'react';
-import AnimalShow from './AnimalShow';
 
-function getRandomAnimal() {
-  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
-  return animals[Math.floor(Math.random() * animals.length)];
-}
 
 function App() {
-  const [animals, setAnimals] = useState([]);
 
-  const handleClick = () => {
-    setAnimals([...animals, getRandomAnimal()]);
-  };
-
-  const renderedAnimals = animals.map((animal, index) => {
-    return <AnimalShow key={index} type={animal} />;
-  });
 
   return (
-    <div className="app">
-      <button onClick={handleClick}>Add Animal</button>
-      <div className="animal-list">{renderedAnimals}</div>
+    <div>
+      Chapter 5 API
     </div>
   );
 }
