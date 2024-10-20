d79daaf50726bdc742b9dee5ce0df4046844db69
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-20 10:25:49 -0400

    c6-88 Receiving new titles

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 function App() {
 
   const [books,setBooks] = useState([]);
+  const createBook = (title)=>{
+    console.log(title);
+  }
   
   return (
     <div>
-        Chapter 6
+        <BookCreate onCreate={createBook}/>
     </div>
   );
 }
--- learn_jsx/src/components/BookCreate.js
+++ learn_jsx/src/components/BookCreate.js
@@ -1,6 +1,23 @@
-function BookCreate() {
+import { useState } from "react"
+function BookCreate({onCreate}) {
+  const [title,setTitle]=useState('');
+  const handleChange = (event)=>{
+    setTitle(event.target.value)
+  }
+  const handleSubmit=(event)=>{
+    event.preventDefault();  //  The default action of onSubmit will reload the page, we don't want that
+    onCreate(title);
+    setTitle(''); //clear the input box
+  }
+  
   return (
-    <div>BookCreate</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange}/>
+        <button>Create</button>
+        </form>
+    </div>
   )
 }
 
