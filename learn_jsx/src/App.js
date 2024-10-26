c9c5b614476bda6801e84c188d0432c15fc9f4d1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-26 09:40:25 -0400

    c6-105 Generating Random Ids

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -7,9 +7,25 @@ function App() {
     //Bad Code Example!
     // books.push({id:123,title:title});
     // setBooks(books);
-    const updatedBooks = [...books,{id:123,title}];
+    const uniqueString = generateUniqueString(10);
+    const updatedBooks = [...books,{id:uniqueString,title}];
     setBooks(updatedBooks);
+    console.log(uniqueString);
   }
+
+  function generateUniqueString(length = 10) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return `${Date.now()}-${result}`;
+}
+
+// Example usage
+const uniqueString = generateUniqueString(10);
+
   
   return (
     <div>
