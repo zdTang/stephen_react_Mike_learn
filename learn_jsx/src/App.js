aaee28757825d2c161c1ad9a9ccb2b346be71452
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-27 11:58:51 -0400

    c7-127 updating a Record

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -36,14 +36,20 @@ function App() {
   }
 
   
-  const editBookById = (id,newTitle)=>{
-    const updatedBooks = books.map(book=>{
-      if(book.id===id){
-        return {...book,title:newTitle}
-      }
-      return book;
+  const editBookById = async (id,newTitle)=>{
+    // const updatedBooks = books.map(book=>{
+    //   if(book.id===id){
+    //     return {...book,title:newTitle}
+    //   }
+    //   return book;
+    // })
+    await axios.put(`http://localhost:3001/books/${id}`,{
+      "title":newTitle
     })
-    setBooks(updatedBooks);
+
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
   }
   const deleteBookById = id =>{
     var afterDeletedBookById = books.filter(book=>book.id !== id);
