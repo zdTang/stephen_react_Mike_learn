72fa47d08d86d1937e28c8118f6a39c8f8c3b8a2
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-29 11:51:47 -0400

    c9-148 Fixing bugs with useCallback

--- learn_jsx/src/context/books.js
+++ learn_jsx/src/context/books.js
@@ -1,18 +1,20 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 const BooksContext = createContext();
 
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
-  };
+  }, []);
+
+  //const stableFetchBooks = useCallback(fetchBooks,[])
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
 
   const createBook = async (title) => {
     const response = await axios.post("http://localhost:3001/books", {
