fb81bf145cdb40c37f4ef185ffaf3dd273f103c5
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-30 15:03:19 -0400

    c10-156 The children prop

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,5 +1,24 @@
+import Button from "./Button";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Button>Big Deal!</Button>
+      </div>
+      <div>
+        <Button>Holy Smoke!</Button>
+      </div>
+      <div>
+        <Button>Trump 2024</Button>
+      </div>
+      <div>
+        <Button>Learn stuff</Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
