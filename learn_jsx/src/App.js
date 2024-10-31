1bf3209c76fa03d01dcf573024cf9a229744144d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-31 15:38:36 -0400

    c10-172 Handing the special className Case

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,22 +1,27 @@
 import Button from "./Button";
 const App = () => {
+  const handleClick = () => {
+    console.log("click");
+  };
   return (
     <div>
       <div>
-        <Button primary>Hello</Button>
+        <Button primary className="mb-8" onClick={handleClick}>
+          Hello
+        </Button>
       </div>
       <div>
-        <Button primary rounded>
+        <Button primary rounded onMouseEnter={handleClick}>
           Big Deal!
         </Button>
       </div>
       <div>
-        <Button success outline rounded>
+        <Button success outline rounded onMouseLeave={handleClick}>
           Holy Smoke!
         </Button>
       </div>
       <div>
-        <Button secondary rounder>
+        <Button secondary rounded>
           Trump 2024
         </Button>
       </div>
--- learn_jsx/src/Button.js
+++ learn_jsx/src/Button.js
@@ -10,24 +10,30 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
-  const classes = className("flex items-center px-3 py-1.5 border", {
-    "border-blue-500 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-black": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-500 bg-red-500 text-white": danger,
-    "rounded-full": rounded,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-900": outline && secondary,
-    "text-green-500": outline && success,
-    "text-black-400": outline && warning,
-    "text-red": outline && danger,
-  });
-
+  console.log(rest);
+  const classes = className(
+    rest.className,
+    "flex items-center px-3 py-1.5 border",
+    {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-black": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-black-400": outline && warning,
+      "text-red": outline && danger,
+    }
+  );
+  console.log(classes);
   return (
-    <button className={classes}>
+    <button {...rest} className={classes}>
       {children}
       <FaBeer />
     </button>
