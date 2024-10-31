f859e678cc51f5736d9a39e695360b9b83d8bf0c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-31 14:06:38 -0400

    c10-169-adjust margin by adding style

--- learn_jsx/src/Button.js
+++ learn_jsx/src/Button.js
@@ -11,10 +11,10 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className("px-3 py-1.5 border", {
+  const classes = className("flex items-center px-3 py-1.5 border", {
     "border-blue-500 bg-blue-500 text-white": primary,
     "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
+    "border-green-500 bg-green-500 text-black": success,
     "border-yellow-400 bg-yellow-400 text-white": warning,
     "border-red-500 bg-red-500 text-white": danger,
     "rounded-full": rounded,
@@ -23,7 +23,7 @@ function Button({
     "text-gray-900": outline && secondary,
     "text-green-500": outline && success,
     "text-black-400": outline && warning,
-    "text-red-500": outline && danger,
+    "text-red": outline && danger,
   });
 
   return (
