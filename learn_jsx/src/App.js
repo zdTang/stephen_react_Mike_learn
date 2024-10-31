e83a2e8e503c774ef632a9b89d66408ffad5e093
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-10-31 16:41:53 -0400

    c10-175 Refactoring with Organization

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,40 +1,11 @@
-import Button from "./Button";
+import ButtonPage from "./pages/ButtonPage";
 const App = () => {
   const handleClick = () => {
     console.log("click");
   };
   return (
     <div>
-      <div>
-        <Button primary className="mb-8" onClick={handleClick}>
-          Hello
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded onMouseEnter={handleClick}>
-          Big Deal!
-        </Button>
-      </div>
-      <div>
-        <Button success outline rounded onMouseLeave={handleClick}>
-          Holy Smoke!
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded>
-          Trump 2024
-        </Button>
-      </div>
-      <div>
-        <Button warning outline>
-          Learn stuff
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Learn stuff
-        </Button>
-      </div>
+      <ButtonPage />
     </div>
   );
 };
---
+++ learn_jsx/src/pages/ButtonPage.js
@@ -0,0 +1,42 @@
+import Button from "../components/Button";
+const ButtonPage = () => {
+  const handleClick = () => {
+    console.log("click");
+  };
+  return (
+    <div>
+      <div>
+        <Button primary className="mb-8" onClick={handleClick}>
+          Hello
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded onMouseEnter={handleClick}>
+          Big Deal!
+        </Button>
+      </div>
+      <div>
+        <Button success outline rounded onMouseLeave={handleClick}>
+          Holy Smoke!
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Trump 2024
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          Learn stuff
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Learn stuff
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPage;
