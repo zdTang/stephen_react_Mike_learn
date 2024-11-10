3d9d33ebc21e28d49500bae67202efd0dd3cd687
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-10 10:42:19 -0500

    c12-206 Creating the reusable Panel

---
+++ learn_jsx/src/components/Panel.js
@@ -0,0 +1,15 @@
+import classNames from "classnames";
+const Panel = ({ children, className, ...rest }) => {
+  const finalClassNames = classNames(
+    "border rounded p-3 shadow bg-white w-full",
+    className
+  );
+
+  return (
+    <div className={finalClassNames} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+export default Panel;
