c85a48930a0924802a03656eceea8a6d902747f2
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-14 21:03:41 -0500

    c13-224 Navigation Context

--- learn_jsx/src_chapter6-9/index.js
+++ learn_jsx/src_chapter6-9/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "./context/books";
+import { NavigationProvider } from "./context/NavigationContext";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Provider>
-    <App />
-  </Provider>
+  <NavigationProvider>
+    <Provider>
+      <App />
+    </Provider>
+  </NavigationProvider>
 );
