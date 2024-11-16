4e77ce488f041ac411f7a49a7990507cd81da97e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-16 15:49:18 -0500

    C13-230 Link Styling

--- learn_jsx/src/components/Link.js
+++ learn_jsx/src/components/Link.js
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import NavigationContext from "../context/NavigationContext";
+import classNames from "classnames";
 
 const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
+  const classes = classNames("text-blue-500");
   const handleClick = (event) => {
     if (event.metakey || event.ctrlKey) return;
     event.preventDefault();
     navigate(to);
   };
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
