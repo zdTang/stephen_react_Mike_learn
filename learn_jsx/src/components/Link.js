a8a07b010348e2016037b377d038a9ca356ef333
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-16 15:29:20 -0500

    c13- 229 Handling Control and Command keys

--- learn_jsx/src/components/Link.js
+++ learn_jsx/src/components/Link.js
@@ -4,10 +4,15 @@ import NavigationContext from "../context/NavigationContext";
 const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
   const handleClick = (event) => {
+    if (event.metakey || event.ctrlKey) return;
     event.preventDefault();
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
