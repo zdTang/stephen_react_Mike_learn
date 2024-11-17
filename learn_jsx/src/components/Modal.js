fb024b2c409d5d4037a92b3450f2a69303f09b01
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 10:19:01 -0500

    c14-237 At First Glance Easy

--- learn_jsx/src/components/Modal.js
+++ learn_jsx/src/components/Modal.js
@@ -1,5 +1,10 @@
 const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">Hello there!</div>
+    </div>
+  );
 };
 
 export default Modal;
