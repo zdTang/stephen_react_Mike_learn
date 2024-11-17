dc9518276c1086d080e428f01218c10bc74e2d9e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 17:57:52 -0500

    c15-249 Better Styling

--- learn_jsx/src/pages/TablePage.js
+++ learn_jsx/src/pages/TablePage.js
@@ -4,7 +4,7 @@ const TablePage = () => {
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
-    { name: "Lime", color: "green-500", score: 4 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
   return (
