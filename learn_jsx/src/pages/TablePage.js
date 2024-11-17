9f822c9d0aa8d817136804f6b962216149bc876c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 18:58:54 -0500

    c15-256 Fixing the Color

--- learn_jsx/src/pages/TablePage.js
+++ learn_jsx/src/pages/TablePage.js
@@ -9,7 +9,10 @@ const TablePage = () => {
 
   const config = [
     { label: "Name", render: (fruit) => fruit.name },
-    { label: "Color", render: (fruit) => `p-3 m-2 ${fruit.color}` },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
     { label: "Score", render: (fruit) => fruit.score },
   ];
 
