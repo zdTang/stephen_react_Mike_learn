95870d3a118e0efaba35f3aa80a3ac7d76b57c93
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 10:42:45 -0500

    c14-238 Were Lucy it works at all

--- learn_jsx/src/components/Modal.js
+++ learn_jsx/src/components/Modal.js
@@ -1,6 +1,6 @@
 const Modal = () => {
   return (
-    <div>
+    <div className="relative">
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">Hello there!</div>
     </div>
