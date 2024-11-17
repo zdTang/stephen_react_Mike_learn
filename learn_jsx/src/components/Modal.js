0b398ac1d40a257dcb4fd6fc8ed3c241348fbd9f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 11:38:36 -0500

    C14-244 Modal Wrap up-- lock the scroll of body element

--- learn_jsx/src/components/Modal.js
+++ learn_jsx/src/components/Modal.js
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
+
 const Modal = ({ onClose, children, actionBar }) => {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
   return ReactDOM.createPortal(
     <div>
       <div
