42816447ebf611cd87e38ae74c7876d8cc20f6a5
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 17:48:28 -0500

    c15-248 Building the Rows

--- learn_jsx/src/components/Table.js
+++ learn_jsx/src/components/Table.js
@@ -1,5 +1,23 @@
 const Table = ({ data }) => {
-  return <div>{data.length}</div>;
+  const renderedRows = data.map((fruit) => (
+    <tr key={fruit.name}>
+      <td>{fruit.name}</td>
+      <td>{fruit.color}</td>
+      <td>{fruit.score}</td>
+    </tr>
+  ));
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 };
 
 export default Table;
