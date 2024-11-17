229407b33bef2005d1b5adca2f38c28350e3b7f1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-17 10:55:32 -0500

    c14-239 Fixing the Modal with Protal

--- learn_jsx/src/components/Modal.js
+++ learn_jsx/src/components/Modal.js
@@ -1,9 +1,11 @@
+import ReactDOM from "react-dom";
 const Modal = () => {
-  return (
-    <div className="relative">
+  return ReactDOM.createPortal(
+    <div>
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">Hello there!</div>
-    </div>
+    </div>,
+    document.querySelector(".modal-container")
   );
 };
 
