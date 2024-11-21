39e08744a07ae4fed8db22b1de8c679148bc8c59
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-20 21:45:11 -0500

    c16-270 Watching for Header Cell Clicks

--- learn_jsx/src/components/SortableTable.js
+++ learn_jsx/src/components/SortableTable.js
@@ -1,11 +1,18 @@
 import Table from "./Table";
 const SortableTable = (props) => {
   const { config } = props;
+  const handleClick = (label) => {
+    console.log(label);
+  };
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
     return {
       ...column,
-      header: () => <th>{column.label}IS SORTABLE</th>,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label}IS SORTABLE
+        </th>
+      ),
     };
   });
   //...props has a 'config',but it will be overwriten
