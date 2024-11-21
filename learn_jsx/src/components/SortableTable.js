c030501b4f58ee78ff4f0de52137a36e1ec552af
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-20 22:05:59 -0500

    c16-272 Adding sort state

--- learn_jsx/src/components/SortableTable.js
+++ learn_jsx/src/components/SortableTable.js
@@ -1,8 +1,21 @@
 import Table from "./Table";
+import { useState } from "react";
 const SortableTable = (props) => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
   const { config } = props;
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder("null");
+      setSortBy(null);
+    }
   };
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
