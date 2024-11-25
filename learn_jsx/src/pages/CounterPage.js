ed8b9065d5bfe4d744f1cbcf2d01f817702630e8
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-25 10:00:57 -0500

    c17-287-Adding the  Form

--- learn_jsx/src/pages/CounterPage.js
+++ learn_jsx/src/pages/CounterPage.js
@@ -1,24 +1,47 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-Counter";
+import Panel from "../components/Panel";
+import { useState } from "react";
 
 const CounterPage = ({ initialCount }) => {
-  const { count, handleClick } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
-    <div>
-      CounterPage
-      <div>
-        <h1>Count is {count}</h1>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flax flex-row">
         <Button
           success
           className="mb-5 mx-2"
           outline
           rounded
-          onClick={handleClick}
+          onClick={increment}
         >
           Increment
         </Button>
+        <Button
+          success
+          className="mb-5 mx-2"
+          outline
+          rounded
+          onClick={decrement}
+        >
+          Decrement
+        </Button>
       </div>
-    </div>
+      <form>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-3"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 };
 
