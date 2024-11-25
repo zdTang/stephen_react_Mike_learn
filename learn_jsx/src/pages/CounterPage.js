f1bb068d3515b3fa98030da22e89ad3314750cec
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-25 11:53:02 -0500

    c17-290 Understanding Action Objects

--- learn_jsx/src/pages/CounterPage.js
+++ learn_jsx/src/pages/CounterPage.js
@@ -2,10 +2,20 @@ import Button from "../components/Button";
 import Panel from "../components/Panel";
 import { useReducer } from "react";
 function reducer(state, action) {
-  return {
-    ...state,
-    count: state.count + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+  if (action.type === "value_to_add") {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+
+  return state;
 }
 
 const CounterPage = ({ initialCount }) => {
@@ -16,14 +26,14 @@ const CounterPage = ({ initialCount }) => {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(initialCount);
   const increment = () => {
-    dispatch();
+    dispatch({ type: "increment" });
   };
   const decrement = () => {
     // setCount(count - 1);
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0; // if the result of parseInt is false, return 0
-    // setValueToAdd(value);
+    dispatch({ type: "value_to_add", payload: value });
   };
 
   const handleSubmit = (event) => {
