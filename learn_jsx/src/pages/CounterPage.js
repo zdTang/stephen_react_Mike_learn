72b423de9d8355346b64278b73f20de50af9ebfd
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-25 11:36:11 -0500

    c17-289 Rules of Reducer Function

--- learn_jsx/src/pages/CounterPage.js
+++ learn_jsx/src/pages/CounterPage.js
@@ -1,30 +1,40 @@
 import Button from "../components/Button";
 import Panel from "../components/Panel";
-import { useState } from "react";
+import { useReducer } from "react";
+function reducer(state, action) {
+  return {
+    ...state,
+    count: state.count + 1,
+  };
+}
 
 const CounterPage = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(initialCount);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(initialCount);
   const increment = () => {
-    setCount(count + 1);
+    dispatch();
   };
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0; // if the result of parseInt is false, return 0
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flax flex-row">
         <Button
           success
@@ -49,7 +59,7 @@ const CounterPage = ({ initialCount }) => {
         <label>Add a lot!</label>
         <input
           type="number"
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           className="p-1 m-3 bg-gray-50 border border-gray-3"
         />
