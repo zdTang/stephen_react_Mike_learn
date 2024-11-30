625710067323673b5a56dd26e97880d9ef9a071b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-29 21:31:06 -0500

    c19-308 Accessing State in a Component

--- learn_jsx/src/components/SongPlaylist.js
+++ learn_jsx/src/components/SongPlaylist.js
@@ -1,12 +1,15 @@
 import { createRandomSong } from "../data";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addSong } from "../store";
 
 function SongPlaylist() {
   const dispatch = useDispatch();
   // To Do:
   // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state) => {
+    console.log(state);
+    return state.songs;
+  });
 
   const handleSongAdd = (song) => {
     // const action = addSong(song);
