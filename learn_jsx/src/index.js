445f4e8b46507d6965cc74dc2350fa4ded3d4343
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-23 11:38:04 -0500

    c20-328 Creating the Store

--- learn_jsx/src/index.js
+++ learn_jsx/src/index.js
@@ -1,8 +1,13 @@
 import { createRoot } from "react-dom/client";
-
+import { store } from "./store";
 import App from "./App";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
