ed9a58a758a911ad88b01a626ffcc66f6c348951
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-12 05:45:44 -0500

    c20-337 Form reset on submission

--- learn_jsx/src/components/CarForm.js
+++ learn_jsx/src/components/CarForm.js
@@ -25,6 +25,8 @@ function CarForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
     dispatch(addCar({ name, cost }));
+    //dispatch(changeCost(0));
+    //dispatch(changeName(""));
   };
 
   return (
