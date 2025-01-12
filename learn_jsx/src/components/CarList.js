5f5967629b31cafca7d2620f51ddb380be04514e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-11 21:24:40 -0500

    c20-333 Listing the records

--- learn_jsx/src/components/CarList.js
+++ learn_jsx/src/components/CarList.js
@@ -3,8 +3,30 @@ const CarList = () => {
   const cars = useSelector((state) => {
     return state.cars.data;
   });
-  console.log(cars);
-  return <div>CarList</div>;
+  const handleCarDelete = (car) => {
+    console.log(car);
+  };
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name}- {car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 };
 
 export default CarList;
