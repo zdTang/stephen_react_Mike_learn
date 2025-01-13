2bd4e72959b05a465c3c499e747aa68803e09c3a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-13 06:12:06 -0500

    c21-348 Adding a Few Components

--- learn_jsx/src/App.js
+++ learn_jsx/src/App.js
@@ -1,5 +1,10 @@
+import UsersList from "./components/UsersList";
 function App() {
-  return <div>Chapter 21</div>;
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
