4dff49eacfa36132a31481778a8237844e2ef4d1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-16 06:15:55 -0500

    c21-359 Using loading state

--- learn_jsx/src/components/UsersList.js
+++ learn_jsx/src/components/UsersList.js
@@ -1,19 +1,30 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 import store from "../store"; // Import your Redux store instance
 export default function UsersList() {
   const disPatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    console.log("In useSelector, State changed:", state.users);
+    return state.users;
+  });
   useEffect(() => {
     disPatch(fetchUsers());
     //monitor state in the store
     store.subscribe(() => {
-      console.log("State changed:", store.getState());
+      console.log("in Subscribe, State changed:", store.getState());
     });
     // the following approach cannot get real time state from store
     const currentState = store.getState();
     console.log("Second way to see Current state:", currentState); // Print the full Redux state
   }, [disPatch]);
 
-  return <div>UsersList</div>;
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  if (error) {
+    return <div>Error Fetching</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
