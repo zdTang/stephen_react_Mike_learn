c39f30e0d6941f9fa7a6cd3b160406fafafc39d3
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-16 06:38:31 -0500

    C21-360 Adding a Pause for Testing

--- learn_jsx/src/store/thunks/fetchUsers.js
+++ learn_jsx/src/store/thunks/fetchUsers.js
@@ -7,9 +7,18 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3001/users");
-
+  //DEV ONLY
+  await pause(1000);
   return response.data;
 });
+
+// DEV ONLY
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 /*
 Once create a thunk, it will automatically generate three properties:
