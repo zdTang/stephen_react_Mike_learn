f9f6036a36f789e174c1c290ea8d77596876d383
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-19 07:11:03 -0500

    C21-375 Refector the component

--- learn_jsx/src/components/UsersList.js
+++ learn_jsx/src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import useThunk from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 export default function UsersList() {
   // Notice the export name, and understand the common logic for each Thunk
@@ -31,13 +32,7 @@ export default function UsersList() {
     content = <div>Error Fetching</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
