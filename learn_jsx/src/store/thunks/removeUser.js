e03082690417a2d70a1716ca4cece5e47efa53c6
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-19 06:51:30 -0500

    C21-373 A Thunk to delete a User

---
+++ learn_jsx/src/store/thunks/removeUser.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const removeUser = createAsyncThunk("users/remove", async (user) => {
+  const response = await axios.delete(`http://localhost:3001/users/${user.id}`);
+  //FIX !!
+  return response.data;
+});
+
+export { removeUser };
