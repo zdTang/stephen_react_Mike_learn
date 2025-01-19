cbe2b923994dc8ef5128a7f422068df1565f2068
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-19 05:26:47 -0500

    C21-370 Create reuseable Thunk Hook - A

--- learn_jsx/src/components/UsersList.js
+++ learn_jsx/src/components/UsersList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import store from "../store"; // Import your Redux store instance
@@ -6,8 +6,29 @@ import Skeleton from "./Skeleton";
 import Button from "./Button";
 
 export default function UsersList() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  function useThunk(thunk) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const dispatch = useDispatch();
+    const runThunk = useCallback(
+      (arg) => {
+        setIsLoading(true);
+        dispatch(thunk(arg))
+          .unwrap()
+          .catch((err) => setError(err))
+          .finally(() => setIsLoading(false));
+      },
+      [dispatch, thunk]
+    );
+    return [runThunk, isLoading, error];
+  }
+
+  /*   const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null); */
+
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+
   const [isCreatingUser, setIsCreatingUser] = useState(false);
   const [creatingUserError, setCreatingUserError] = useState(null);
   const disPatch = useDispatch();
@@ -16,23 +37,8 @@ export default function UsersList() {
     return state.users;
   });
   useEffect(() => {
-    setIsLoadingUsers(true);
-    disPatch(fetchUsers()) //this is a Promise
-      .unwrap() // After testing, I found this unwrap can be delete?
-      .catch((err) => {
-        setLoadingUsersError(err);
-      })
-      .finally(() => {
-        setIsLoadingUsers(false);
-      });
-    //monitor state in the store
-    store.subscribe(() => {
-      console.log("in Subscribe, State changed:", store.getState());
-    });
-    // the following approach cannot get real time state from store
-    const currentState = store.getState();
-    console.log("Second way to see Current state:", currentState); // Print the full Redux state
-  }, [disPatch]);
+    doFetchUsers();
+  }, []);
 
   const handleUserAdd = () => {
     setIsCreatingUser(true);
