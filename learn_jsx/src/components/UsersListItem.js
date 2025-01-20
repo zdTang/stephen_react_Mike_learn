48ba7d0209ed5532ad957a94cc2c02b1f5bec679
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-20 05:15:31 -0500

    C21-380 Adding the ExpandablePanel

--- learn_jsx/src/components/UsersListItem.js
+++ learn_jsx/src/components/UsersListItem.js
@@ -2,24 +2,22 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import useThunk from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
   const handleClick = () => {
     doRemoveUser(user);
   };
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <GoTrash />
-          </Button>
-          {error && <div>Error deleting user</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <GoTrash />
+      </Button>
+      {error && <div>Error deleting user</div>}
+      {user.name}
+    </>
   );
+  return <ExpandablePanel header={header}>CONTENT!</ExpandablePanel>;
 }
 
 export default UsersListItem;
