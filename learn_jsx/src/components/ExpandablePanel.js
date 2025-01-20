1007372655a42fb3fe680df0dcf0e5ab641eb57f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-20 05:32:43 -0500

    C21-381 Wrapping Up the ExpendablePanel

--- learn_jsx/src/components/ExpandablePanel.js
+++ learn_jsx/src/components/ExpandablePanel.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function ExpandablePanel({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+  const content = <div className="p-2 border-t">{children}</div>;
   return (
     <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="flex p-2 justify-between items-center">
         <div className="flex flex-row items-center justify-between">
           {header}
         </div>
+        <div onClick={() => setExpanded(!expanded)} className="cursor-pointer">
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {expanded && content}
     </div>
   );
 }
