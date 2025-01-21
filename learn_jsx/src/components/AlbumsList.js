ef83a15a8c59d0c3355c5751bf82e18b17f078a0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-21 07:22:50 -0500

    C22-390 Rendering the List

--- learn_jsx/src/components/AlbumsList.js
+++ learn_jsx/src/components/AlbumsList.js
@@ -1,8 +1,29 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
 const AlbumsList = ({ user }) => {
-  //const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  const results = useFetchAlbumsQuery(user);
-  console.log(results);
-  return <div>Album for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} className="h-10 w-full" />;
+  } else if (error) {
+    content = <div>Error Fetching</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of Photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+  return (
+    <div>
+      <div>Album for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 };
 export default AlbumsList;
