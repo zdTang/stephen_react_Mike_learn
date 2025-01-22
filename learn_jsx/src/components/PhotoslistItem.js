b459471d2c96cb1280b2089e54c041d75ed56ecb
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-22 10:05:28 -0500

    C22-410-A  Display Photo

--- learn_jsx/src/components/PhotoslistItem.js
+++ learn_jsx/src/components/PhotoslistItem.js
@@ -1,7 +1,7 @@
 function PhotosListItem({ photo }) {
   return (
-    <div>
-      <h1>PhotosListItem</h1>
+    <div className="relative m-2">
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
     </div>
   );
 }
