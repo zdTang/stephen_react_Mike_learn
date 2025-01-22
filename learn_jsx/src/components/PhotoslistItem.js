f819717ba192e70bc069b3de5f4ad222cd2b9e64
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-01-22 10:16:13 -0500

    Revert "C22-40-B Adding Mouseover delete"
    This reverts commit 5d22d248369b2141657dcc8d7664f8b5172dc778.

--- learn_jsx/src/components/PhotoslistItem.js
+++ learn_jsx/src/components/PhotoslistItem.js
@@ -1,16 +1,7 @@
-import { useRemovePhotosMutation } from "../store";
-import { GoTrash } from "react-icons/go";
 function PhotosListItem({ photo }) {
-  const [removePhoto /* , results */] = useRemovePhotosMutation();
-  const handleRemovePhoto = () => {
-    removePhoto(photo);
-  };
   return (
-    <div onClick={handleRemovePhoto} className="relative cursor m-2">
+    <div className="relative m-2">
       <img className="h-20 w-20" src={photo.url} alt="random pic" />
-      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
-        <GoTrash className="text-3xl" />
-      </div>
     </div>
   );
 }
